818d5a878350007ddca4b9fc3f5de830d6b7815a
tyage <namatyage@gmail.com>
2024-08-14 00:31:38 +0900

    Update redir.py

--- redir.py
+++ redir.py
@@ -1,3 +1,2 @@
-a = f'<meta http-equiv="refresh" content="0;url=//tyage.net">clickthisaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa<!--'
-a = f'<div style="position: absolute; top: -200px; left: -200px; bottom: -200px; right: -200px; background: red; z-index: 10000; font-size: 10em; color: yellow; padding: 150px; visibility: initial;">WARNING!!!</div><!--'
+a = f'<meta http-equiv="refresh" content="0;url=//tyage.net"><!--'
 clickthis
